0a5b8663c440975798b515ab3e3f9335644619e0
macdh <macdhollister@gmail.com>
2018-11-01 02:15:08 -0700

    Added html, styling, and initial javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+    game = {
+        numCorrect: 0,
+        numIncorrect: 0,
+        numSkipped: 0,
+        correctAnswer: "",
+        userAnswer: "",
+        currentTimeLeft: 20,
+        // other stuff?
+    }
+
+    $(".answer").click(function(event) {
+        game.userAnswer = event.target.id;
+
+        if (event.target.id == game.correctAnswer) {
+            alert("correct!");
+            // This will change to "correct" screen
+        } else {
+            alert("incorrect");
+            // This will change to "incorrect" screen
+        }
+
+    });
+
+    function resetGame() {
+        game.numCorrect = 0;
+        game.numIncorrect = 0;
+        game.numSkipped = 0;
+        // update other stuff in game if needed
+    }
+
+    function newQuestion(q) {
+
+        q = questionsData[q];
+        $("#questionText").text(q.question);
+        $("#a").text(q.a);
+        $("#b").text(q.b);
+        $("#c").text(q.c);
+        $("#d").text(q.d);
+        game.currentTimeLeft = 20;
+        game.correctAnswer = q.correct;
+    }
+
+    newQuestion(1);
+})
+
+
+
+/*
+game object:
+    correct answers
+    incorrect answers
+    skipped answers
+    current currect answer
+    current time left
+
+Also possibly keep track of answers in the game object instead, then use a
+update screen function to change what the displayed text is
+
+Keep track of correct answer (in game object -- "a", "b", "c", or "d")
+newQuestion updates correct answer
+
+
+*/
---
+++ assets/javascript/questions.js
@@ -0,0 +1,19 @@
+let questionsData = [
+    {
+        "question" : "Who is the former manager of Dunder Mifflin Stamford?",
+        "a"        : "Josh Porter",
+        "b"        : "Jan Levinson",
+        "c"        : "Charles Miner",
+        "d"        : "Deangelo Vickers",
+        "correct"  : "a"
+    },
+    
+    {
+        "question" : "In S1E1 \"Pilot\": Who started their first day at Dunder Mifflin Scranton?",
+        "a"        : "Jim Halpert",
+        "b"        : "Ryan Howard",
+        "c"        : "Michael Scott",
+        "d"        : "Erin Hannon",
+        "correct"  : "b"
+    },
+]
